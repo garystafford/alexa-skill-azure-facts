b3d80280f152ddda655c920c9db8917d4f952dc6
Gary A. Stafford <garystafford@rochester.rr.com>
2018-07-24 21:08:52 -0400

    Initial commit of project for post

---
+++ README.md
@@ -0,0 +1,84 @@
+# Azure Tech Facts Alexa Custom Skill
+
+Demonstration skill for post, [Building Asynchronous, Serverless Alexa Skills with AWS Lambda, DynamoDB, S3, and Node.js](http://www.programmaticponderings.com). Explores the creation of an Alexa Custom Skill, using the latest Alexa Skills Kit, the AWS Serverless Platform, and Node.js.
+
+## About the Skill
+
+Ask Alexa technical facts about Microsoft Azure, the cloud computing service created by Microsoft for building, testing, deploying, and managing applications and services through a global network of Microsoft-managed data centers. Current facts include Certifications, Cognitive Services, Competition, Compliance, Description, First Product, Geographies, Global Presence, Platforms, Product Categories, Products, Regions, Release Date.
+
+## Deployment and Testing
+
+```bash
+export AWS_ACCESS_KEY_ID=<YOUR_AWS_ACCESS_KEY_ID>
+export AWS_SECRET_ACCESS_KEY=<YOUR_AWS_SECRET_ACCESS_KEY>
+ask init # create default profile
+ask deploy --profile default
+```
+
+## Create DynamoDB Table
+
+```bash
+aws dynamodb create-table \
+  --table-name AzureFacts \
+  --attribute-definitions \
+    AttributeName=Fact,AttributeType=S \
+  --key-schema AttributeName=Fact,KeyType=HASH \
+  --provisioned-throughput ReadCapacityUnits=3,WriteCapacityUnits=3
+```
+
+## Import data into DynamoDB
+
+```bash
+aws dynamodb batch-write-item \
+  --request-items file://data/AzureFacts.json
+```
+
+## Create S3 Bucket
+
+```bash
+aws s3api create-bucket \
+  --bucket <my_bucket_name> \
+  --region us-east-1
+```
+
+## Simulate Skill
+
+```bash
+ask simulate \
+  --text "Load Azure Tech Facts" \
+  --locale "en-US" \
+  --skill-id "<your_skill_id>" \
+  --profile "default"
+```
+
+## Sample Invocations
+
+#### One-Shot Invocation
+
+-   "Alexa, ask Azure Tech Facts for a random fact for Gary"
+-   "Alexa, ask Azure Tech Facts to tell Michele about global infrastructure"
+-   "Alexa, ask Azure Tech Facts about certifications for Shawn"
+
+#### 4-part Invocation
+
+-   "Alexa, load Azure Tech Facts"
+-   "Ask a question"
+-   "My name is Alice"
+-   "Tell me about certifications"
+
+#### 3-part Invocation
+
+-   "Alexa, ask Azure Tech Facts for a fact"
+-   "My name is Matt"
+-   "When was Azure released"
+
+#### 2-part Invocation
+
+-   "Alexa, ask Azure Tech Facts about Azure's Cognitive Services"
+-   "My name is Frank"
+
+## References
+
+-   <https://developer.amazon.com/docs/smapi/skill-manifest.html>
+-   <https://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_UpdateItem.html>
+-   <https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/GettingStarted.NodeJs.03.html#GettingStarted.NodeJs.03.04>
---
+++ lambda/custom/index.js
@@ -0,0 +1,198 @@
+"use strict";
+const Alexa = require("ask-sdk-core");
+const AWS = require("aws-sdk");
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+/* CONSTANTS */
+const SKILL_NAME = "Azure Tech Facts";
+const CARD_TITLE = `Welcome to ${SKILL_NAME}`;
+const FACTS_ARRAY = ["description", "released", "global", "regions", "geographies", "platforms", "categories", "products", "cognitive", "compliance", "first", "certifications", "competition"];
+const FACTS_LIST = "Certifications, Cognitive Services, Competition, Compliance, First Products, Geographies, Global Presence, Platforms, Product Categories, Products, Regions, and Release Date";
+const BUCKET_URL = "https://s3.amazonaws.com/alexa-skills-gstafford"
+
+const IMAGES = {
+    smallImageUrl: `${BUCKET_URL}/azure-108x108.png`,
+    largeImageUrl: `${BUCKET_URL}/azure-512x512.png`
+};
+
+/* INTENT HANDLERS */
+const LaunchRequestHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === "LaunchRequest";
+    },
+    handle(handlerInput) {
+        speechOutput = `Welcome to ${SKILL_NAME}. To start, say, ask a question, or, give me a fact. To list the available facts, say help.`;
+        repromptspeechOutput = speechOutput;
+        cardContent = `${speechOutput}\nAvailable facts include ${FACTS_LIST}.`;
+
+        return handlerInput.responseBuilder
+            .speak(speechOutput)
+            .reprompt(repromptspeechOutput)
+            .withStandardCard(CARD_TITLE, cardContent,
+                IMAGES.smallImageUrl, `${BUCKET_URL}\/image-01.png`)
+            .getResponse();
+    },
+};
+
+const AzureFactsIntent = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === "IntentRequest"
+            && handlerInput.requestEnvelope.request.intent.name === "AzureFactsIntent";
+    },
+    async handle(handlerInput) {
+        let currentIntent;
+
+        let myName = slotValue(handlerInput.requestEnvelope.request.intent.slots.myName);
+        let myQuestion = slotValue(handlerInput.requestEnvelope.request.intent.slots.myQuestion);
+
+        if (!myName) {
+            currentIntent = myName;
+            return handlerInput.responseBuilder
+                .addDelegateDirective(currentIntent)
+                .getResponse();
+        }
+
+        // return a random fact...
+        if (myQuestion === "random") {
+            myQuestion = FACTS_ARRAY[Math.floor(Math.random() * FACTS_ARRAY.length)];
+        }
+
+        if (!myQuestion) {
+            currentIntent = myQuestion;
+            return handlerInput.responseBuilder
+                .addDelegateDirective(currentIntent)
+                .getResponse();
+        }
+
+        let fact = await buildFactResponse(myName, myQuestion);
+        myName = Object.is(myName, undefined) ? undefined : capitalizeFirstLetter(myName);
+        console.log(`myName: ${myName}`);
+        console.log(`myQuestion: ${myQuestion}`);
+        let factToSpeak = `${myName}, ${fact.Attributes.Response}`
+        console.log(factToSpeak);
+        cardContent = factToSpeak;
+        return handlerInput
+            .responseBuilder
+            .speak(factToSpeak)
+            .withStandardCard(CARD_TITLE, cardContent,
+                IMAGES.smallImageUrl, `${BUCKET_URL}\/${fact.Attributes.Image}`)
+            .getResponse();
+    },
+};
+
+const HelpIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === "IntentRequest"
+            && handlerInput.requestEnvelope.request.intent.name === "AMAZON.HelpIntent";
+    },
+    handle(handlerInput) {
+        speechOutput = `Current facts include: ${FACTS_LIST}.`;
+        repromptspeechOutput = "To start, say, ask a question. To list the available facts, say help.";
+        cardContent = speechOutput;
+
+        return handlerInput.responseBuilder
+            .speak(speechOutput)
+            .reprompt(repromptspeechOutput)
+            .withStandardCard(CARD_TITLE, cardContent, IMAGES.smallImageUrl, IMAGES.largeImageUrl)
+            .getResponse();
+    },
+};
+
+const CancelAndStopIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === "IntentRequest"
+            && (handlerInput.requestEnvelope.request.intent.name === "AMAZON.CancelIntent"
+                || handlerInput.requestEnvelope.request.intent.name === "AMAZON.StopIntent");
+    },
+    handle(handlerInput) {
+        speechOutput = "Goodbye!";
+        cardContent = speechOutput;
+
+        return handlerInput.responseBuilder
+            .speak(speechOutput)
+            .withStandardCard(CARD_TITLE, cardContent, IMAGES.smallImageUrl, IMAGES.largeImageUrl)
+            .getResponse();
+    },
+};
+
+const SessionEndedRequestHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === "SessionEndedRequest";
+    },
+    handle(handlerInput) {
+        //any cleanup logic goes here
+        return handlerInput.responseBuilder.getResponse();
+    },
+};
+
+const ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        console.log(`Error handled: ${error.message}`);
+        speechOutput = `Sorry, I cannot understand the command. Please say again.`;
+        repromptspeechOutput = speechOutput;
+        return handlerInput.responseBuilder
+            .speak(speechOutput)
+            .reprompt(repromptspeechOutput)
+            .getResponse();
+    },
+};
+
+/* HELPER FUNCTIONS */
+let cardContent, speechOutput, repromptspeechOutput;
+
+function slotValue(slot, useId) {
+    let value = slot.value;
+    let resolution = (slot.resolutions && slot.resolutions.resolutionsPerAuthority
+        && slot.resolutions.resolutionsPerAuthority.length > 0) ? slot.resolutions.resolutionsPerAuthority[0] : null;
+    if (resolution && resolution.status.code === "ER_SUCCESS_MATCH") {
+        let resolutionValue = resolution.values[0].value;
+        value = resolutionValue.id && useId ? resolutionValue.id : resolutionValue.name;
+    }
+    return value;
+}
+
+// Format names which come over as all lowercase from Alexa
+function capitalizeFirstLetter(name) {
+    return name.charAt(0).toUpperCase() + name.slice(1);
+}
+
+// Increments hits by 1 and returns values all in one call to DynamoDB
+function buildFactResponse(myName, myQuestion) {
+    return new Promise((resolve, reject) => {
+        if (myQuestion !== undefined) {
+            let params = {};
+            params.TableName = "AzureFacts";
+            params.Key = {"Fact": myQuestion};
+            params.UpdateExpression = "set Hits = Hits + :val";
+            params.ExpressionAttributeValues = {":val": 1};
+            params.ReturnValues = "ALL_NEW";
+
+            docClient.update(params, function (err, data) {
+                if (err) {
+                    console.log("GetItem threw an error:", JSON.stringify(err, null, 2));
+                    reject(err);
+                } else {
+                    console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+                    resolve(data);
+                }
+            });
+        }
+    });
+}
+
+/* LAMBDA SETUP */
+const skillBuilder = Alexa.SkillBuilders.custom();
+
+exports.handler = skillBuilder
+    .addRequestHandlers(
+        LaunchRequestHandler,
+        AzureFactsIntent,
+        HelpIntentHandler,
+        CancelAndStopIntentHandler,
+        SessionEndedRequestHandler
+    )
+    .addErrorHandlers(ErrorHandler)
+    .lambda();
